165c8f8ea47668bfba0b4ee9cceaaa6bf656a3e8
Aaron Burmeister <f34r91@gmail.com>
2019-12-30 18:33:33 +0000

    Automatically backed up by Learn

--- fix_using_tests/turtles_all_the_way_down.rb
+++ fix_using_tests/turtles_all_the_way_down.rb
@@ -2,7 +2,7 @@
 def turtles
     [
       {name: "Donatello", weapon: "bo-staff", traits: ["serious", "leader"]},
-      {name: "Michaelangelo", weapon: "nunchuks", traits: "party dude"},
+      {name: "Michaelangelo", weapon: "nunchuks", traits: ["party dude"]},
       {name: "Raphael", weapon: "sais", traits: ["cool", "rude"]},
       {name: "Leonardo", weapon: "katanas", traits: ["loves being a turtle"]}
     ]
@@ -10,10 +10,6 @@ end
 
 def turtle_traits(turtles)
   turtles.map do |turtle|
-    if turtle[:traits].class == String
-      [] << turtle[:traits]
-    else
-      turtle[:traits]
-    end
+    turtle[:traits]
   end
 end
